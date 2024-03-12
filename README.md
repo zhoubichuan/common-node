f8c88c25d638904b7491db8a3b29716fa07a92e5
zhoubichuan <zhoubichuan@icloud.com>
2024-03-12 22:43:00 +0800

    更新服务

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # common-node
+
+纯node开发案例，不使用相关node框架
+
 公共服务
---
+++ app.js
@@ -0,0 +1,13 @@
+const http = require("http")
+const example = require('./example/index.js')
+const student = require('./student/index.js')
+const port = "3000"
+const server = http.createServer()
+server.listen(port, function () {
+    console.log(`已经连接${port}端口`)
+})
+
+server.on("request", function (req, res) {
+    example(req, res)
+    student(req, res)
+})
