7a61596115fb80a75a570cd82e44c26121485c16
周必川 <zhoubichuan@icloud.com>
2025-06-07 18:21:27 +0800

    更新接口

--- router/example/index.js
+++ router/example/index.js
@@ -1,18 +1,24 @@
 const fs = require("fs")
 
 let example = (req, res) => {
-    console.log('example', req.url)
-    fs.readFile(__dirname + '/example/query', (err, result) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.writeHead(200, {
-                "Content-Type": "text/html",
+    const key = req.url.split('/').pop()
+    const router = {
+        query(req, res) {
+            console.log('example', req.url)
+            fs.readFile(__dirname + '/example', (err, result) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    res.writeHead(200, {
+                        "Content-Type": "text/html",
+                    })
+                    result = result.toString()
+                    res.write(JSON.stringify({ result, code: 200 }))
+                    res.end()
+                }
             })
-            console.log("a", result.toString())
-            res.write(JSON.stringify({ result, code: 200 }))
-            res.end()
         }
-    })
+    }
+    router[key](req, res)
 }
 module.exports = example
